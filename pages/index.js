e4ac2213d9b6993b61c550477ce4484f251f804b
Avery King <avery.n.king@wmich.edu>
2022-03-19 18:07:44 -0400

    ``Footer`` added

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <div>
+      <Navbar/>
       <h1>Home</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi dolorem earum eius explicabo id iure modi, pariatur quaerat voluptate voluptatem! Assumenda doloremque inventore ipsam, magnam nesciunt rerum ut voluptates voluptatum?</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi dolorem earum eius explicabo id iure modi, pariatur quaerat voluptate voluptatem! Assumenda doloremque inventore ipsam, magnam nesciunt rerum ut voluptates voluptatum?</p>
+        <Footer/>
     </div>
   )
 }
