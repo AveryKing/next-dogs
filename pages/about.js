33b704312735bc47504be83c5397d534046749cd
Avery King <avery.n.king@wmich.edu>
2022-03-19 17:55:11 -0400

    ``About`` added

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda consectetur eum facere fugiat incidunt iste iure, maiores nihil non nulla porro quisquam recusandae sapiente sequi totam velit, voluptate voluptatum?</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda consectetur eum facere fugiat incidunt iste iure, maiores nihil non nulla porro quisquam recusandae sapiente sequi totam velit, voluptate voluptatum?</p>
+        </div>
+    );
+}
+
+export default About;
