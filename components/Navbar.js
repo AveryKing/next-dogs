a14132c399119fe03a7a3c5a24287d3d3f540af7
Avery King <avery.n.king@wmich.edu>
2022-03-19 18:06:09 -0400

    ``Navbar`` added

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className='logo'>
+                <h1>Dog List</h1>
+            </div>
+            <a>Home</a>
+            <a>About</a>
+            <a>Dogs List</a>
+        </nav>
+    );
+};
+
+export default Navbar;
