a2fe5be24851f6d277bc493f84e1c4288f7cb323
Avery King <avery.n.king@wmich.edu>
2022-03-19 18:13:46 -0400

    dogs link added to ``index.js``

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link';
 import styles from '../styles/Home.module.css'
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -11,6 +12,9 @@ export default function Home() {
       <h1>Home</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi dolorem earum eius explicabo id iure modi, pariatur quaerat voluptate voluptatem! Assumenda doloremque inventore ipsam, magnam nesciunt rerum ut voluptates voluptatum?</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi dolorem earum eius explicabo id iure modi, pariatur quaerat voluptate voluptatem! Assumenda doloremque inventore ipsam, magnam nesciunt rerum ut voluptates voluptatum?</p>
+        <Link href={'/dogs'}>
+            <a>See dog list</a>
+        </Link>
         <Footer/>
     </div>
   )
