3f698127cffb1420270d9eb2b1e9de8a0bb62a7d
Avery King <avery.n.king@wmich.edu>
2022-03-19 20:42:52 -0400

    styles added to dogs page

--- pages/dogs/index.js
+++ pages/dogs/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import styles from '../../styles/Dogs.module.css';
 export const getStaticProps = async () => {
     const res = await fetch('https://api.thedogapi.com/v1/breeds?api_key=api_key=0b0b0e18-213c-42c5-8f1f-fbeb5e09efe5&limit=10');
     const data = await res.json();
@@ -11,6 +11,13 @@ const index = ({dogs}) => {
     return (
         <div>
             <h1>All Dogs</h1>
+            {dogs.map(dog => (
+                <div key={dog.id}>
+                    <a className={styles.single}>
+                        <h3>{dog.name}</h3>
+                    </a>
+                </div>
+            ))}
         </div>
     );
 };
