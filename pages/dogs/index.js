2c2ddfb4e738427ac445ac069d6fe9807b9f7f86
Avery King <avery.n.king@wmich.edu>
2022-03-19 20:36:11 -0400

    getStaticProps added

--- pages/dogs/index.js
+++ pages/dogs/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
-const index = () => {
+export const getStaticProps = async () => {
+    const res = await fetch('https://api.thedogapi.com/v1/breeds?api_key=api_key=0b0b0e18-213c-42c5-8f1f-fbeb5e09efe5&limit=10');
+    const data = await res.json();
+    return {
+        props: {dogs: data}
+    }
+}
+const index = ({dogs}) => {
     return (
         <div>
             <h1>All Dogs</h1>
