4d43dc1ab6903b0fe824bf5265419d3bc3c1012c
Avery King <avery.n.king@wmich.edu>
2022-03-19 21:38:11 -0400

    getStaticPaths added

--- pages/dogs/[id].js
+++ pages/dogs/[id].js
@@ -1,11 +1,23 @@
 import React from 'react';
 
+export const getStaticPaths = async () => {
+    const res = await fetch('https://api.thedogapi.com/v1/breeds?api_key=api_key=0b0b0e18-213c-42c5-8f1f-fbeb5e09efe5&limit=10');
+    const data = await res.json();
+    const paths = data.map(dog => {
+        return {
+            params: {id: dog.id.toString()}
+        }
+    })
+    return {paths, fallback: false}
+
+}
+
 const Details = () => {
     return (
         <div>
-            <h1>Details Page</h1>
+            <h1>Details</h1>
         </div>
     );
 };
 
-    export default Details;
+export default Details;
