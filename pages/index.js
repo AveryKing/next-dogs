6a277b5cd4028e9584b159c4a8e987adcf964a4c
Avery King <avery.n.king@wmich.edu>
2022-03-20 00:17:45 -0400

    build;

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
             <div>
                 <h1 className={styles.title}>Home</h1>
                 <p className={styles.text}>Dogs are domesticated mammals, not natural wild animals. They were originally bred from wolves. They have been bred by humans for a long time, and were the first animals ever to be domesticated. There are different studies that suggest that this happened between 15.000 and 100.000 years before our time. </p>
-                <p className={styles.text}>Dogs have lived and worked with humans in many roles. In addition to dogs' role as companion animals, dogs have been bred for herding livestock (collies, sheepdogs), hunting (hounds, pointers) and rodent control (terriers). Other types of working dogs include search and rescue dogs, detection dogs trained to detect illicit drugs or chemical weapons; guard dogs; dogs who assist fishermen with the use of nets; and dogs that pull loads.</p>
+                <p className={styles.text}>Dogs have lived and worked with humans in many roles. In addition to the roles dog serve as companion animals, they have been bred for herding livestock (collies, sheepdogs), hunting (hounds, pointers) and rodent control (terriers). Other types of working dogs include search and rescue dogs, detection dogs trained to detect illicit drugs or chemical weapons; guard dogs; dogs who assist fishermen with the use of nets; and dogs that pull loads.</p>
                 <Link href={'/dogs'}>
                     <a className={styles.btn}>See Dogs</a>
                 </Link>
