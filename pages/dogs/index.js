6eee3d6917a08f30acc6cc104982660c5682a913
Avery King <avery.n.king@wmich.edu>
2022-03-19 21:20:39 -0400

    dog links added

--- pages/dogs/index.js
+++ pages/dogs/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from '../../styles/Dogs.module.css';
 import Head from "next/head";
+import Link from 'next/link';
 export const getStaticProps = async () => {
     const res = await fetch('https://api.thedogapi.com/v1/breeds?api_key=api_key=0b0b0e18-213c-42c5-8f1f-fbeb5e09efe5&limit=10');
     const data = await res.json();
@@ -18,11 +19,11 @@ const index = ({dogs}) => {
         <div>
             <h1>All Dogs</h1>
             {dogs.map(dog => (
-                <div key={dog.id}>
+                <Link href={`/dogs/${dog.id}`} key={dog.id}>
                     <a className={styles.single}>
                         <h3>{dog.name}</h3>
                     </a>
-                </div>
+                </Link>
             ))}
         </div>
         </>
